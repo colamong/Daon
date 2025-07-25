e923a286eede9b29f81317f96f8aaa26595ce560
minseok <mjang.dev@gmail.com>
2025-07-25 13:19:46 +0900

    feat: backend ground project

---
+++ BE/src/test/java/com/daon/be/BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daon.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
