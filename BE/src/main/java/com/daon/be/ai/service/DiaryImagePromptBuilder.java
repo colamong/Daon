d7cafabe12fd14a15a023472d8efb2e45ba048bb
HOJEONGKIMM <khj15263245@gmail.com>
2025-08-04 14:24:04 +0900

    feat: 이미지 생성 프롬프트 작성

--- BE/src/main/java/com/daon/be/ai/service/DiaryImagePromptBuilder.java
+++ BE/src/main/java/com/daon/be/ai/service/DiaryImagePromptBuilder.java
@@ -7,6 +7,6 @@ public class DiaryImagePromptBuilder {
     public String build(String summary) {
         
         // 스타일, 화풍, 톤 등 모두 여기에
-        return "아이가 그린 것처럼 크레파스를 사용해서 그린 그림체로, 따뜻하고 귀엽게 그려줘. " + summary;
+        return "아이가 그린 것처럼 크레파스를 사용해서 따뜻하고 귀엽게 그려줘. " + summary;
     }
 }
