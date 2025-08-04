dc8dd406c8bece68df265f020157d2df84268040
HOJEONGKIMM <khj15263245@gmail.com>
2025-08-04 14:27:38 +0900

    feat: 불필요한 코드 삭제

--- BE/src/main/java/com/daon/be/child/controller/ChildExpressionController.java
+++ BE/src/main/java/com/daon/be/child/controller/ChildExpressionController.java
@@ -1,7 +1,6 @@
 package com.daon.be.child.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,16 +33,4 @@ public class ChildExpressionController {
 
 		return ResponseEntity.ok(response);
 	}
-
-
-	// 그림일기 월별 조회
-	@GetMapping("/{childId}/diary")
-	public ResponseEntity<?> getChildDiary(@PathVariable Long childId) {
-		// 나중에 그림일기 조회 서비스 호출
-		return ResponseEntity.ok("그림일기 결과 반환");
-	}
-
-	// 그림일기 단건 조회
-
-	//
 }
