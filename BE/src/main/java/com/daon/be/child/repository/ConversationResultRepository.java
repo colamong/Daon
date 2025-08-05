221dd6c13fc32e97e62090c3c3fdf5c31c9b6aa1
HOJEONGKIMM <khj15263245@gmail.com>
2025-08-05 14:29:51 +0900

    fix: GMS 기반 sttText 요약 및 감정 분석 로직 수정

--- BE/src/main/java/com/daon/be/child/repository/ConversationResultRepository.java
+++ BE/src/main/java/com/daon/be/child/repository/ConversationResultRepository.java
@@ -1,8 +1,15 @@
 package com.daon.be.child.repository;
 
+import java.util.Optional;
+
 import com.daon.be.conversation.entity.ConversationResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConversationResultRepository extends JpaRepository<ConversationResult, Long> {
+
+	// 특정 아이가 특정 주제에 대해 대화 결과 이미 등록했는지 여부를 확인
 	boolean existsByChildIdAndTopicId(Long childId, Long topicId);
+
+	// 특정 아이의 특정 주제에 해당하는 대화 결과를 조회
+	Optional<ConversationResult> findByChildIdAndTopicId(Long childId, Long topicId);
 }
