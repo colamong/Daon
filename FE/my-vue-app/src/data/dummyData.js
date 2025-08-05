626ac572f97f740cf51a5bdfc2875a10945855ef
임유나 <libbyyouna@gmail.com>
2025-08-05 14:07:18 +0900

    refactor: data file create

---
+++ FE/my-vue-app/src/data/dummyData.js
@@ -0,0 +1,39 @@
+// id 는 고유한 숫자
+export const dummyEvents = [
+  {
+    id: 1,
+    date: "2025-08-06",
+    title: "유치원 운동회 준비",
+    description: "유치원 운동회 전날 도시락 장보기",
+  },
+  {
+    id: 2,
+    date: "2025-08-06",
+    title: "저거 뭐냐 무시짱이",
+    description: "친구와 함께하는 무시짱 놀이 시간입니다.",
+  },
+  {
+    id: 3,
+    date: "2025-08-07",
+    title: "또 다른 일정",
+    description: "다른 재미있는 활동이 예정되어 있습니다.",
+  },
+  {
+    id: 4,
+    date: "2025-08-08",
+    title: "마지막 일정",
+    description: "마지막 날 일정을 마무리합니다.",
+  },
+  {
+    id: 5,
+    date: "2025-08-29",
+    title: "잠에 들고 싶어요",
+    description: "하루 종일 피곤했어요.",
+  },
+  {
+    id: 6,
+    date: "2025-09-4",
+    title: "잠에 들고 싶어요",
+    description: "하루 종일 피곤했어요.",
+  },
+];
