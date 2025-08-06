1b7206552d30d101839258eb4cbfeed140eb7e14
임유나 <libbyyouna@gmail.com>
2025-08-06 09:13:50 +0900

    feat: child select

--- FE/my-vue-app/src/router/index.js
+++ FE/my-vue-app/src/router/index.js
@@ -95,7 +95,7 @@ const routes = [
   },
 
   // 4) 아이 전용 (헤더·푸터 없이)
-  { path: "/child", name: "ChildMain", component: ChildMain },
+  { path: "/child/:childId?", name: "ChildMain", component: ChildMain, props: true },
   { path: "/child/pet", name: "ChildPet", component: ChildPet },
   { path: "/child/drawing", name: "ChildDrawing", component: ChildDrawing },
 
