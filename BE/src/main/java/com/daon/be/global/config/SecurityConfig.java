42cce1cd1de550be2352955ece5936bb85659a61
ãhighjjw <highjjw@naver.com>
2025-08-07 17:52:51 +0900

    chore: SecurityConfig 설정

--- BE/src/main/java/com/daon/be/global/config/SecurityConfig.java
+++ BE/src/main/java/com/daon/be/global/config/SecurityConfig.java
@@ -23,33 +23,25 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-			.csrf(csrf -> csrf.disable())
-			.cors(cors -> {}) // CORS 설정은 WebConfig에서 함
-			.authorizeHttpRequests(auth -> auth
-					//.anyRequest().permitAll()
-				.requestMatchers(
-					"/api/user/signup",
-					"/api/user/signin",
-					"/api/user/nations",
-					"/api/user/nation/**",
-					"/oauth2/**",
-					"/login/**"
-				).permitAll()
-				.anyRequest().authenticated()
-			)
-			.exceptionHandling(ex -> ex
-				.authenticationEntryPoint((request, response, authException) -> {
-					response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
-				})
-			)
-			.oauth2Login(oauth2 -> oauth2
-				.successHandler(oAuth2SuccessHandler)
-			)
-			// JWT 필터를 UsernamePasswordAuthenticationFilter 앞에 추가
-			.addFilterBefore(
-				new JwtAuthenticationFilter(jwtUtil),
-				UsernamePasswordAuthenticationFilter.class
-			);
+				.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/**").permitAll()
+						// .requestMatchers("/oauth2/**", "/login/**", "/api/user/**").permitAll()
+						.anyRequest().authenticated()
+				)
+				.exceptionHandling(ex -> ex
+						.authenticationEntryPoint((request, response, authException) -> {
+							response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+						})
+				)
+				.oauth2Login(oauth2 -> oauth2
+						.successHandler(oAuth2SuccessHandler)
+				)
+				// JWT 필터를 UsernamePasswordAuthenticationFilter 앞에 추가
+				.addFilterBefore(
+						new JwtAuthenticationFilter(jwtUtil),
+						UsernamePasswordAuthenticationFilter.class
+				);
 
 		return http.build();
 	}
