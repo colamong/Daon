d7ef7dd8ea46a3fac96ac58d2d102f82a897570b
minseok <mjang.dev@gmail.com>
2025-08-07 14:31:24 +0900

    fix: 모든 상황에서 로그인 필요 없도록 설정(개발완료 후 재설정 예정)

--- BE/src/main/java/com/daon/be/global/config/SecurityConfig.java
+++ BE/src/main/java/com/daon/be/global/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,9 +18,18 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.csrf(csrf -> csrf.disable())
+			.cors(cors -> {}) // CORS 설정은 WebConfig에서 함
 			.authorizeHttpRequests(auth -> auth
-				.requestMatchers("/oauth2/**", "/login/**", "/api/user/**").permitAll()
-				.anyRequest().authenticated()
+					.anyRequest().permitAll()
+				/*.requestMatchers(
+					"/api/user/signup",
+					"/api/user/signin",
+					"/api/user/nations",
+					"/api/user/nation/**",
+					"/oauth2/**",
+					"/login/**"
+				).permitAll()
+				.anyRequest().authenticated()*/
 			)
 			.oauth2Login(oauth2 -> oauth2
 				.successHandler(oAuth2SuccessHandler)
