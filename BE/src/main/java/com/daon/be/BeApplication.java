f2df136aa5ebe50b376468351bda36b1451422ef
ãhighjjw <highjjw@naver.com>
2025-08-17 01:27:05 +0900

    Update BeApplication.java

--- BE/src/main/java/com/daon/be/BeApplication.java
+++ BE/src/main/java/com/daon/be/BeApplication.java
@@ -1,8 +1,11 @@
 package com.daon.be;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableAsync
@@ -12,4 +15,9 @@ public class BeApplication {
         SpringApplication.run(BeApplication.class, args);
     }
 
+    @PostConstruct
+    public void started() {
+        // JVM 전체 타임존을 KST로 고정
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
